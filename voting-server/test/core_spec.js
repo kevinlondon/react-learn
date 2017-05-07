955ea79fcfa1f62deab809ad06f6b21bf7d0592b
Kevin London <kevinlondon@gmail.com>
2017-05-07 13:23:49 -0600

    Refactor the vote method to not need additional state

--- voting-server/test/core_spec.js
+++ voting-server/test/core_spec.js
@@ -109,46 +109,34 @@ describe('application logic', () => {
 
     it('creates a tally for the voted entry', () => {
       const state = Map({
-        vote: Map({
-          pair: initialMovies,
-        }),
-        entries: List()
+        pair: initialMovies,
       });
       const nextState = vote(state, 'Trainspotting');
 
       expect(nextState).to.equal(Map({
-        vote: Map({
-          pair: initialMovies,
-          tally: Map({
-            'Trainspotting': 1
-          })
-        }),
-        entries: List()
+        pair: initialMovies,
+        tally: Map({
+          'Trainspotting': 1
+        })
       }));
     });
 
     it('adds to existing tally for the voted entry', () => {
       const state = Map({
-        entries: List(),
-        vote: Map({
-          pair: initialMovies,
-          tally: Map({
-            'Trainspotting': 3,
-            '28 Days Later': 2
-          })
+        pair: initialMovies,
+        tally: Map({
+          'Trainspotting': 3,
+          '28 Days Later': 2
         })
       });
 
       const nextState = vote(state, 'Trainspotting');
       expect(nextState).to.equal(Map({
-        vote: Map({
-          pair: initialMovies,
-          tally: Map({
-            'Trainspotting': 4,
-            '28 Days Later': 2
-          })
-        }),
-        entries: List()
+        pair: initialMovies,
+        tally: Map({
+          'Trainspotting': 4,
+          '28 Days Later': 2
+        })
       }));
     });
 
