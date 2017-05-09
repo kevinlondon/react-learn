d2dedd96aa9d10614e04938775f396c59488ccec
Kevin London <kevinlondon@gmail.com>
2017-05-09 13:04:20 -0700

    Working through the React Router section

--- fullstack-redux-tutorial/README.md
+++ fullstack-redux-tutorial/README.md
@@ -1,4 +1,4 @@
 # Fullstack Redux Tutorial
 
-Follows http://teropa.info/blog/2015/09/10/full-stack-redux-tutorial.html#broadcasting-state-from-a-redux-listener
+Follows http://teropa.info/blog/2015/09/10/full-stack-redux-tutorial.html
 as best as possible.
--- fullstack-redux-tutorial/voting-client/src/components/Results.jsx
+++ fullstack-redux-tutorial/voting-client/src/components/Results.jsx
@@ -4,8 +4,6 @@ import PureRenderMixin from 'react-addons-pure-render-mixin';
 export default React.createClass({
   mixins: [PureRenderMixin],
   render: function() {
-    return <div className="winner">
-      Winner is {this.props.winner}!
-    </div>;
+    return <div>Hello from results!</div>
   }
 });
