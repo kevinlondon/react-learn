6e3f89cd89e188a2c3ccd62703c396ac1d64ccac
Kevin London <kevinlondon@gmail.com>
2017-05-10 13:03:03 -0700

    Up to Introducing a Client-Side Redux Store

--- fullstack-redux-tutorial/voting-client/src/components/App.jsx
+++ fullstack-redux-tutorial/voting-client/src/components/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import {List} from 'immutable';
+import {List, Map} from 'immutable';
 
 const pair = List.of('Trainspotting', '28 Days Later');
+const tally = Map({'Trainspotting': 5, '28 Days Later': 4});
 
 export default React.createClass({
   render: function() {
