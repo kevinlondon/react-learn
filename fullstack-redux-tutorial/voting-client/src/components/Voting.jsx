4b860dd2e924731aadd452c779dfcc973e86f472
Kevin London <kevinlondon@gmail.com>
2017-05-18 13:59:16 -0700

    Down to Sending Actions to the Server

--- fullstack-redux-tutorial/voting-client/src/components/Voting.jsx
+++ fullstack-redux-tutorial/voting-client/src/components/Voting.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
+import {connect} from 'react-redux';
 import Winner from './Winner';
 import Vote from './Vote';
+import * as actionCreators from '../action_creators';
 
-
-export default React.createClass({
+export const Voting = React.createClass({
   mixins: [PureRenderMixin],
   render: function() {
     return (
@@ -17,3 +18,13 @@ export default React.createClass({
     );
   }
 });
+
+function mapStateToProps(state) {
+  return {
+    pair: state.getIn(['vote', 'pair']),
+    hasVoted: state.get('hasVoted'),
+    winner: state.get('winner')
+  };
+}
+
+export const VotingContainer = connect(mapStateToProps, actionCreators)(Voting);
